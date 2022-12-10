bb0a2c59bcd683fe72df4e738d778f7a43742239
Shaima Alkhateeb <shaimaalkhateeb535@gmail.com>
2022-12-10 13:41:18 +0200

    add react bootstrap for signin && signup

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { login } from '../actions/AuthActions'
+import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
+import '../css/Sign.css'
 
 export default function SignIn() {
     const dispatch = useDispatch();
@@ -8,14 +11,27 @@ export default function SignIn() {
   return (
     <div>
         <h1>Sign In</h1>
-        <form onSubmit={(e)=> login(e, dispatch)}>
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" />
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
-            <button type="submit">Sign In</button>
+        <Form onSubmit={(e)=> login(e, dispatch)} className='theForm'>
+          
+            <Form.Group className='formGroup'>
+              <Form.Label className='label'>Email</Form.Label>
+              <Form.Control type="email" name="email" placeholder='Email' id="email" className='input' required />
+            </Form.Group>
 
-        </form>
+            <Form.Group className='formGroup'>
+              <Form.Label className='label'>Password</Form.Label>
+              <Form.Control type="password" name="password" placeholder='Password' id="password" className='input' required />
+            </Form.Group>
+
+            <p>
+            You don't have an account? <a href="/signup">Sign up now</a>
+            </p>
+
+            <Button variant="outline-success" className="submitButton" type="submit">
+              Sign In
+            </Button>
+
+        </Form>
     </div>
   )
 }
