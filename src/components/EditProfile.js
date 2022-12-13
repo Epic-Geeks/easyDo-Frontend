2f1d26917273535a1d95fd1d6224f7c42319a093
AliMohammad420 <aliwessam509@gmail.com>
2022-12-13 16:35:22 +0000

    Fixing the provider file

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -18,19 +18,28 @@ export default function EditProfile() {
 
   return (
     <div>
-      <h2>Add Address</h2>
+       <div className="flex items-center justify-center min-h-screen ">
+        <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 ">
+        <h2 className="text-3xl font-bold dark:text-white">Add Address</h2>
       <form onSubmit={(e) => editProfile(e, dispatch, userInfo)}>
-        <label>City</label>
-        <input type="text" name="city" id="city" />
-        <label>Street</label>
-        <input type="text" name="street" id="street" />
-        {/* <label>password</label>
-            <input type="text" name="password" id="password" /> */}
-        <label>building num</label>
-        <input type="text" name="buildingNum" id="buildingNum" />
-        <button type="submit">Add Address</button>
+      <div className="mb-6">
+    <label htmlFor="city" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">City</label>
+    <input type="text" id="city" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-half p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+</div>
+        <div className="mb-6">
+    <label htmlFor="street" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Street</label>
+    <input type="text" id="street" name="street" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-half p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+</div>
+        
+<div className="mb-6">
+    <label htmlFor="buliding_num" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Buliding num</label>
+    <input type="text" id="buliding_num" name="buliding_num" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-half p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+</div>
+
+
+<button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Address</button>
       </form>
-      {userInfo &&
+       {userInfo &&
         userInfo.customerAddress &&
         userInfo.customerAddress.map((item, idx) => {
           let parsedData = JSON.parse(item);
@@ -42,7 +51,10 @@ export default function EditProfile() {
               <button onClick={() => handleRemoveLoc(idx)}>delete</button>
             </div>
           );
-        })}
+        })} 
+        </div>
+        </div>
     </div>
+
   );
 }
