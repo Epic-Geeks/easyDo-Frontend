a72fc25defe9b7c3e89126db4e920aefdbe2dde2
AliMohammad420 <aliwessam509@gmail.com>
2022-12-19 07:38:54 +0000

    Finished Services Just call the modal I don't have time

--- src/components/provider/MyServices.js
+++ src/components/provider/MyServices.js
@@ -50,6 +50,8 @@ export default function MyServices() {
       Edit Service
       </button>
   
+  /* Call the Modal component */
+  
 
     
         <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-5" onClick={(e) => deleteService(dispatch, service.id)}>Delete Service</button>
